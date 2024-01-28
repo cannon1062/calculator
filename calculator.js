5e54920e03dca059f9c55615c9f42cfe2964e748
Brook <37387041+cannon1062@users.noreply.github.com>
2024-01-27 19:37:22 -0500

    Create initial docs and variables

---
+++ calculator.js
@@ -0,0 +1,19 @@
+let firstNumber = prompt('enter first number');
+let operator = prompt('enter operator');
+let secondNumber = prompt('enter second number');
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
