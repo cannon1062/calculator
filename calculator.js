92f5aff5937ab8d8a1026160288d0a5a8d66c5f7
Brook <37387041+cannon1062@users.noreply.github.com>
2024-01-28 14:01:17 -0500

    Create calculator layout

--- calculator.js
+++ calculator.js
@@ -1,6 +1,27 @@
-let firstNumber = prompt('enter first number');
-let operator = prompt('enter operator');
-let secondNumber = prompt('enter second number');
+// let firstNumber = +prompt('enter first number');
+// let operator = prompt('enter operator');
+// let secondNumber = +prompt('enter second number');
+
+// console.log(operate(firstNumber, secondNumber, operator));
+
+function operate(firstNumber, secondNumber, operator) {
+    switch(operator) {
+        case '+':
+            return add(firstNumber, secondNumber);
+
+        case '-':
+            return subtract(firstNumber, secondNumber);
+
+        case '*':
+            return multiply(firstNumber, secondNumber);
+            
+        case '/':
+            return divide(firstNumber, secondNumber);
+
+        default:
+            return "Invalid Operator";
+    }
+}
 
 function add(a, b) {
     return a + b;
