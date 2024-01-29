892bcb28f30ca0b80f7dee4cd4d91dad9d95ae86
Brook <37387041+cannon1062@users.noreply.github.com>
2024-01-28 21:19:42 -0500

    Add number entry capability

--- calculator.js
+++ calculator.js
@@ -1,8 +1,27 @@
-// let firstNumber = +prompt('enter first number');
-// let operator = prompt('enter operator');
-// let secondNumber = +prompt('enter second number');
+const display = document.querySelector('.display-area');
+const numButtons = document.querySelectorAll('.num-button');
+const clearButton = document.querySelector('.clear-button');
+const decimalButton = document.querySelector('.decimal-button');
 
-// console.log(operate(firstNumber, secondNumber, operator));
+numButtons.forEach((numButton) => {
+    numButton.addEventListener('click', () => {
+        if (display.textContent === '0') {
+            display.textContent = numButton.getAttribute('value');
+        } else if(display.textContent.length < 12) {
+            display.textContent += numButton.getAttribute('value');
+        }
+    })
+})
+
+decimalButton.addEventListener('click', () => {
+    if (!display.textContent.includes('.')) {
+        display.textContent += '.';
+    }
+})
+
+clearButton.addEventListener('click', () => {
+    display.textContent = '0';
+})
 
 function operate(firstNumber, secondNumber, operator) {
     switch(operator) {
